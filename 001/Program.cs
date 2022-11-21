a2b2421e3907ffa4988d1861a3cd2623a9b709fa
Haman692 <haman692@yandex.ru>
2022-11-22 00:46:04 +0700

    Квадрат

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(x * x);
