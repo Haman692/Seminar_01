25498bbadedfe9ac31abb9604b58e2a83852e12e
Haman692 <haman692@yandex.ru>
2022-11-22 01:40:19 +0700

    2 задача

---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите перове число"); 
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число"); 
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB == numberA*numberA)
+{
+    Console.WriteLine($"Число {numberB} являеться квадратом {numberA}"); 
+}
+else
+{
+    Console.WriteLine($"Число {numberB} не являеться квадратом {numberA}"); 
+}
