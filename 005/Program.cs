ffbcba8fb671b2a11ef03bd9d0f20fed9536f9f5
Haman692 <haman692@yandex.ru>
2022-11-22 02:37:43 +0700

    5 задача

---
+++ 005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число "); 
+int numberA = Convert.ToInt32(Console.ReadLine());
+int LastNumber = numberA % 10;
+if (99 < numberA)
+{
+    if (numberA < 1000) Console.WriteLine ($"Последняя цифра числа {numberA} - {LastNumber}");
+    else Console.WriteLine ("Неверное число");
+}
+else Console.WriteLine ("Неверное число");
