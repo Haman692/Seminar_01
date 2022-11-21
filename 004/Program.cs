f31b82d70122722cfe227395483210c86a1c6444
Haman692 <haman692@yandex.ru>
2022-11-22 02:26:22 +0700

    4 задача

---
+++ 004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число "); 
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = -numberA;
+while (count < numberA + 1)
+{
+    Console.Write($"{count}, ");
+    count++;
+}
